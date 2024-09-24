7a4d21cd21f10d6ef9200808b8dc171147feba9b
filippogiatti <150442166+filippogiatti@users.noreply.github.com>
2024-09-24 17:32:17 +0200

    Update es8.c

--- es8.c
+++ es8.c
@@ -1,41 +1,39 @@
 /*Progettare un algoritmo che risolve il seguente problema. Si desidera 
 calcolare la somma delle radici quadrate di N valori numerici inseriti 
-dall’utente, con N inserito in input. 
-L’algoritmo deve stampare la somma calcolata. L’algoritmo deve terminare
+dallâ€™utente, con N inserito in input. 
+Lâ€™algoritmo deve stampare la somma calcolata. Lâ€™algoritmo deve terminare
 con un messaggio di errore quando viene inserito un numero che non
 permette di effettuare il calcolo (nel dominio dei numeri reali).*/
 
 #include <stdio.h>
+
 #include <math.h>
 
-int main(int argc, char *argv[]){
-	int numeroNumeri;
-	double somma=0;
-	int numero;
-	int i;
-	
-	do{
-		printf("quanti numeri vuoi inserire?\n");
-		scanf("%d", &numeroNumeri);
-	}while(numeroNumeri < 1);
-	
-	for(i=0; i<numeroNumeri; i++){
-		printf("Inserisci un numero: \n");
-		scanf("%d", &numero);
+int main(int argc, char * argv[]) {
+  int numeroNumeri;
+  double somma = 0;
+  int numero;
+  int i;
 
-        if (numero >= 0)
-        {
+  do {
+    printf("quanti numeri vuoi inserire?\n");
+    scanf("%d", & numeroNumeri);
+  } while (numeroNumeri < 1);
 
-            somma = somma + sqrt(numero);
-        }
-        else
-        {
+  for (i = 0; i < numeroNumeri; i++) {
+    printf("Inserisci un numero: \n");
+    scanf("%d", & numero);
 
-            printf("Hai inserito un numero negativo\n");
-        }
+    if (numero >= 0) {
 
-	}
-	printf("La somma e'': %f", somma);
-	
-	return 0;
+      somma = somma + sqrt(numero);
+    } else {
+
+      printf("Hai inserito un numero negativo\n");
+    }
+
+  }
+  printf("La somma e'': %f", somma);
+
+  return 0;
 }
