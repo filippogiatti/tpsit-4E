5b3ddcefc22d2dcbb7c01fa75368eec5bef2af18
filippogiatti <150442166+filippogiatti@users.noreply.github.com>
2024-12-01 12:36:35 +0100

    Update esercizioAlbero.c

--- esercizioAlbero.c
+++ esercizioAlbero.c
@@ -4,36 +4,38 @@
 int main(int argc, char *argv[])
 {
     int pid1, pid2;
-    printf("Sono il processo P1");
+    printf("Sono il processo P1\n");
     pid1 = fork();
 
-    if(pid1==0){
-        printf("Sono nel processo P2");
+    if(pid1 == 0) {
+        printf("Sono nel processo P2\n");
 
-        int pid1_figlio=fork();
+        int pid1_figlio = fork();
         pid2 = fork();
-        if(pid1_figlio==0){
-            printf("Sono nel processo P4");
 
+        if(pid1_figlio == 0) {
+            printf("Sono nel processo P4\n");
         }
-    }else{
-        pid2=fork;
+    } else {
+        pid2 = fork();
 
-        if(pid2==0){
-            printf("Sono nel processo P3";);
-        }else{
-            int pid2_figlio1=fork();
-            if(pid2_figlio1==0){
-                printf("Sono nel processo P5");
-            }
-        }else{
-            int pid2_figlio2=fork();
-            if(pid2_figlio2==0){
-                printf("sono nel processo P6");
+        if(pid2 == 0) {
+            printf("Sono nel processo P3\n");
+        } else {
+            int pid2_figlio1 = fork();
+            if(pid2_figlio1 == 0) {
+                printf("Sono nel processo P5\n");
+            } else {
+                int pid2_figlio2 = fork();
+                if(pid2_figlio2 == 0) {
+                    printf("Sono nel processo P6\n");
+                }
             }
         }
-    }else{
-        printf("Sono il processo padre");
+    }
+    
+    if (pid1 != 0 && pid2 != 0) {
+        printf("Sono il processo padre\n");
     }
     
     return 0;
